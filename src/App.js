3e48330c32c3dd23ff169ada0c527e8ec5916531
Abdelsalam Habara <ahab0008@student.monash.edu>
2022-12-27 22:54:27 +1100

    finished navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div >
-      <h2 className="text-2xl ">Hi, I'm a React App</h2>
+      <Navbar />
     </div>
   );
 }
