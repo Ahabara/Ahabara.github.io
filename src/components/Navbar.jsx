f1795e7cd5f4577145f7e7371cc56fcb6d774f86
Abdelsalam Habara <ahab0008@student.monash.edu>
2022-12-28 20:00:41 +1100

    changed mobile nav bar bg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,13 @@ import {FaBars, FaTimes} from "react-icons/fa";
 const Navbar = () => {
     const [nav, setNav] = useState(false);
     const handleClick = () => {
-      setNav(!nav);
+        setNav(!nav);
     }
     return (
-        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a1925] text-gray-300">
+        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#006fad] text-gray-300">
             <div>
                 <img src={Logo} alt="logo" style={{width: '50px'}}/>
             </div>
-            {/*menu*/}
                 <ul className="hidden md:flex">
                     <li>Home</li>
                     <li>About</li>
@@ -20,12 +19,10 @@ const Navbar = () => {
                     <li>Work</li>
                     <li>Contact</li>
                 </ul>
-            {/*hamburger*/}
             <div onClick={handleClick} className="md:hidden z-10 cursor-pointer">
                 {!nav ? <FaBars/> : <FaTimes/>}
             </div>
-            {/*{mobile}*/}
-            <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#0a1925] flex flex-col justify-center items-center"}>
+            <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-mobile-nav bg-cover flex flex-col justify-center items-center text-zinc-900"}>
                 <li className="py-6 text-4xl">Home</li>
                 <li className="py-6 text-4xl">About</li>
                 <li className="py-6 text-4xl">Skills</li>
