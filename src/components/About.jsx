2b36d9a4c2cd57db6f658f3d6115b12eae7022a2
Abdelsalam Habara <ahab0008@student.monash.edu>
2023-01-02 23:45:58 +1100

    filled info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,24 +3,26 @@ import React from 'react';
 const About = () => {
     return (
         <div name='about' className="w-full h-screen bg-gradient-to-b from-[#006fad] to-pink-400 text-gray-300">
-        <div className="flex flex-col justify-center items-center w-full h-full">
-            <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
-                <div className="sm:text-left pb-8 pl-4">
-                    <p className="text-4xl font-bold inline border-b-4 border-pink-600">
-                        About
-                    </p>
+            <div className="flex flex-col justify-center items-center w-full h-full">
+                <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+                    <div className="sm:text-left pb-8 pl-4">
+                        <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+                            About
+                        </p>
+                    </div>
+                    <div></div>
                 </div>
-                <div></div>
-            </div>
                 <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
-                    <div className="sm:text right text-4xl font-bold">
+                    <div className="sm:text-right inline text-4xl font-bold ">
                         <p>
-                            Lorem ipsum dolor
+                            A Bit About Me
                         </p>
                     </div>
                     <div>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu elementum quam. Donec ultricies ipsum nulla, et congue enim bibendum sed.
+                            My name is Abdel Habara. I'm a 24 year old IT student passionate about all things software
+                            and finance. Here are a few of my achievements. For more information feel free to <a
+                            style={{color: '#ffffff'}} href='mailto:abdelsalam.habara1997@gmail.com'> email me!</a>
                         </p>
                     </div>
                 </div>
