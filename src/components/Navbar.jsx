11f0d54b211b874d8cf188c796de2f2ca7b25c6d
Abdelsalam Habara <ahab0008@student.monash.edu>
2023-03-16 20:29:17 +1100

    fixed nav bar on mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,9 +11,6 @@ const Navbar = () => {
         console.log("pressed");
     };
 
-    const handleClick2 = () => {
-        console.log("heeey")
-    };
     return (
         // border-2 border-b-fuchsia-400
         <div
@@ -28,18 +25,24 @@ const Navbar = () => {
                     </Link>
                 </li>
                 <li>
-                    <Link  to={'about'}smooth={true} duration={500}>
+                    <Link  to={'about'} smooth={true} duration={500}>
                         About
                     </Link>
                 </li>
                 <li>
-                    <Link name={"Work"} to={'work'}smooth={true} duration={500}/>
+                    <Link  to={'work'} smooth={true} duration={500}>
+                        Work
+                    </Link>
                 </li>
                 <li>
-                    <Link name={"Skills"} to={'skills'}smooth={true} duration={500}/>
+                    <Link  to={'skills'} smooth={true} duration={500}>
+                        Skills
+                    </Link>
                 </li>
                 <li>
-                    <Link name={"Contact"} to={'contact'}smooth={true} duration={500}/>
+                    <Link  to={'contact'} smooth={true} duration={500}>
+                        Contact
+                    </Link>
                 </li>
             </ul>
             <div onClick={handleClick} className="md:hidden z-10 cursor-pointer">
@@ -47,19 +50,29 @@ const Navbar = () => {
             </div>
             <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-mobile-nav bg-cover flex flex-col justify-center items-center text-zinc-900"}>
                 <li>
-                    <Link onClick={handleClick} name={"Home"} to={'home'}smooth={true} duration={500}/>
+                    <Link onClick={handleClick} to={'home'} smooth={true} duration={500}>
+                        Home
+                    </Link>
                 </li>
                 <li>
-                    <Link onClick={handleClick} name={"About"} to={'about'}smooth={true} duration={500}/>
+                    <Link onClick={handleClick}  to={'about'} smooth={true} duration={500}>
+                        About
+                    </Link>
                 </li>
                 <li>
-                    <Link onClick={handleClick} name={"Work"} to={'work'}smooth={true} duration={500}/>
+                    <Link onClick={handleClick} to={'work'} smooth={true} duration={500}>
+                        Work
+                    </Link>
                 </li>
                 <li>
-                    <Link onClick={handleClick} name={"Skills"} to={'skills'}smooth={true} duration={500}/>
+                    <Link onClick={handleClick}  to={'skills'} smooth={true} duration={500}>
+                        Skills
+                    </Link>
                 </li>
                 <li>
-                    <Link onClick={handleClick} name={"Contact"} to={'contact'}smooth={true} duration={500}/>
+                    <Link  onClick={handleClick} to={'contact'} smooth={true} duration={500}>
+                        Contact
+                    </Link>
                 </li>
             </ul>
         </div>
